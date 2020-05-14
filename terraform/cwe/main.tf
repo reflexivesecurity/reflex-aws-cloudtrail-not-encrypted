0fd129db27eeb8cda5c5ab94b0d585b702b30285
rjulian <richard@rjulian.net>
2020-05-14 15:57:58 -0700

    Refactor for multiregion

---
+++ terraform/cwe/main.tf
@@ -0,0 +1,25 @@
+module "cwe" {
+  source      = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe?ref=v0.6.0"
+  name        = "CloudtrailNotEncrypted"
+  description = "Detect if a Cloudtrail trail does not encrypt log files. "
+
+  event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.cloudtrail"
+  ],
+  "detail-type": [
+    "AWS API Call via CloudTrail"
+  ],
+  "detail": {
+    "eventSource": [
+      "cloudtrail.amazonaws.com"
+    ],
+    "eventName": [
+      "CreateTrail",
+      "UpdateTrail"
+    ]
+  }
+}
+PATTERN
+}
