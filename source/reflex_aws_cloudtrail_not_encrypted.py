d2d89f1f7f064fb3cd5a215532e26d9e179eab58
rjulian <richard@rjulian.net>
2020-09-21 00:32:58 -0700

    Fix for event payload parsing

--- source/reflex_aws_cloudtrail_not_encrypted.py
+++ source/reflex_aws_cloudtrail_not_encrypted.py
@@ -26,26 +26,24 @@ class CloudtrailNotEncrypted(AWSRule):
         Return True if it is compliant, and False if it is not.
         """
 
-        response = self.client.describe_trails(
-            trailNameList=[
-                self.trail_name
-            ]
-        )
-        if "KmsKeyId" in response['trailList'][0].keys():
-            return bool(response['trailList'][0]["KmsKeyId"])
+        response = self.client.describe_trails(trailNameList=[self.trail_name])
+        if "KmsKeyId" in response["trailList"][0].keys():
+            return bool(response["trailList"][0]["KmsKeyId"])
         return False
 
     def get_remediation_message(self):
         """ Returns a message about the remediation action that occurred """
-        return f"The trail named {self.trail_name} does not have encryption " \
-               f"enabled. "
+        return (
+            f"The trail named {self.trail_name} does not have encryption " f"enabled. "
+        )
 
 
 def lambda_handler(event, _):
     """ Handles the incoming event """
     print(event)
-    if subscription_confirmation.is_subscription_confirmation(event):
-        subscription_confirmation.confirm_subscription(event)
+    event_payload = json.loads(event["Records"][0]["body"])
+    if subscription_confirmation.is_subscription_confirmation(event_payload):
+        subscription_confirmation.confirm_subscription(event_payload)
         return
-    rule = CloudtrailNotEncrypted(json.loads(event["Records"][0]["body"]))
+    rule = CloudtrailNotEncrypted(event_payload)
     rule.run_compliance_rule()
